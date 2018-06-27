b13d94c965f60391f40b05976151eeca94c95ccc
eugenenew80 <eugenenew80@gmail.com>
2018-06-27 23:04:43 +0600

    -

---
+++ src/main/java/bln.fin/soap/purchase/package-info.java
@@ -0,0 +1,6 @@
+@XmlSchema(namespace = "http://bis.kegoc.kz/soap", elementFormDefault=XmlNsForm.QUALIFIED)
+package bln.fin.soap.purchase;
+
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
+
