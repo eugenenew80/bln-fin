fa5122a9d1052d08da3fa59f3a541e3dffacba72
eugenenew80 <eugenenew80@gmail.com>
2018-07-15 17:40:28 +0600

    -

--- src/main/java/bln/fin/App.java
+++ src/main/java/bln/fin/App.java
@@ -4,16 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import javax.xml.datatype.DatatypeConfigurationException;
 
 @EntityScan(
     basePackageClasses = { App.class, Jsr310JpaConverters.class }
 )
-@EnableScheduling
 @SpringBootApplication
 public class App  {
-    public static void main(String[] args) throws DatatypeConfigurationException {
+    public static void main(String[] args)  {
         SpringApplication.run(App.class, args);
     }
 }
