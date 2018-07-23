1adf7e242231b0c3dcd3657925a00907f98a8595
eugenenew80 <eugenenew80@gmail.com>
2018-07-23 12:22:55 +0600

    -

--- src/main/java/bln/fin/ws/client/plan/SalePlanServiceImpl.java
+++ src/main/java/bln/fin/ws/client/plan/SalePlanServiceImpl.java
@@ -4,6 +4,8 @@ import bln.fin.entity.SalePlanHeader;
 import bln.fin.entity.SalePlanLine;
 import bln.fin.repo.SalePlanHeaderRepo;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.ws.client.core.WebServiceTemplate;
 import org.springframework.ws.soap.client.SoapFaultClientException;
@@ -14,51 +16,58 @@ import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 import static bln.fin.common.Util.toXMLGregorianCalendar;
+import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toList;
+import static java.util.stream.IntStream.range;
 
 @Service
 @RequiredArgsConstructor
 public class SalePlanServiceImpl implements SalePlanService {
     private final SalePlanHeaderRepo salePlanHeaderRepo;
     private final WebServiceTemplate salePlanServiceTemplate;
+    private static final int groupCount = 100;
+    private static final Logger logger = LoggerFactory.getLogger(SalePlanServiceImpl.class);
 
     @Override
     public void send(Long headerId) {
         List<SalePlanHeader> headers = Arrays.asList(salePlanHeaderRepo.findOne(headerId));
-        List<SalesPlan.Item> items = createItems(headers);
-        if (items.isEmpty())
-            return;
-
-        SalesPlan salesPlanReq = new ObjectFactory().createSalesPlan();
-        salesPlanReq.getItem().addAll(items);
-        request(headers, salesPlanReq);
+        request(headers);
     }
 
     @Override
     public void sendAll() {
         List<SalePlanHeader> headers = salePlanHeaderRepo.findAll();
+        request(headers);
+    }
+
+    private void request(List<SalePlanHeader> headers) {
         List<SalesPlan.Item> items = createItems(headers);
         if (items.isEmpty())
             return;
 
-        SalesPlan salesPlanReq = new ObjectFactory().createSalesPlan();
-        salesPlanReq.getItem().addAll(items);
-        request(headers, salesPlanReq);
-    }
+        int i = 0;
+        for (List<SalesPlan.Item> groupItems : splitItems(items)) {
+            i++;
+            logger.info("Batch started: " + i);
 
-    private void request(List<SalePlanHeader> headers, SalesPlan salesPlanReq) {
-        try {
-            Object response = salePlanServiceTemplate.marshalSendAndReceive(salesPlanReq);
-            updateHeaders(headers);
-        }
-        catch (SoapFaultClientException e) {
-            System.out.println("Fault Code: " + e.getFaultCode());
-            System.out.println("Fault Reason: " + e.getFaultStringOrReason());
-            System.out.println("Message: " + e.getLocalizedMessage());
-        }
+            SalesPlan salesPlanReq = new ObjectFactory().createSalesPlan();
+            salesPlanReq.getItem().addAll(groupItems);
 
-        catch (Exception e) {
-            e.printStackTrace();
+            try {
+                Object response = salePlanServiceTemplate.marshalSendAndReceive(salesPlanReq);
+                updateHeaders(headers);
+            }
+            catch (SoapFaultClientException e) {
+                System.out.println("Fault Code: " + e.getFaultCode());
+                System.out.println("Fault Reason: " + e.getFaultStringOrReason());
+                System.out.println("Message: " + e.getLocalizedMessage());
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+            finally {
+                logger.info("Batch completed: " + i);
+            }
         }
     }
 
@@ -83,6 +92,19 @@ public class SalePlanServiceImpl implements SalePlanService {
         item.setStartDate(toXMLGregorianCalendar(header.getStartDate()));
         item.setEndDate(toXMLGregorianCalendar(header.getEndDate()));
         return item;
+    }
+
+    private List<List<SalesPlan.Item>> splitItems(List<SalesPlan.Item> list) {
+        return range(0, list.size())
+            .boxed()
+            .collect(groupingBy(index -> index / groupCount))
+            .values()
+            .stream()
+            .map(indices -> indices
+                .stream()
+                .map(list::get)
+                .collect(toList()))
+            .collect(toList());
     }
 
     private void updateHeaders(List<SalePlanHeader> headers) {
