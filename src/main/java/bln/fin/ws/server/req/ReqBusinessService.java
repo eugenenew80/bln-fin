dbad1840e8c742a38da0ede6ab5e41e0d11346b8
eugenenew80 <eugenenew80@gmail.com>
2018-09-23 23:01:50 +0600

    -

--- src/main/java/bln/fin/ws/server/req/ReqBusinessService.java
+++
@@ -1,9 +0,0 @@
-package bln.fin.ws.server.req;
-
-import bln.fin.entity.ReqLine;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface ReqBusinessService {
-    ReqLine createReqLine(ReqLineDto lineDto);
-}
